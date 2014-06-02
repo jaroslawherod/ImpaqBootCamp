725b10465aabd00d19b081fefa3246417d1035fc
jaroslaw.herod@cumulocity.com <jaro@localhost.localdomain>
2014-06-02 10:48:23 +0200

    hello world

---
+++ hello-world/src/main/java/boot/camp/day1/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+package boot.camp.day1.hello;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class HelloWorld {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+
+
+    }
+
+}
