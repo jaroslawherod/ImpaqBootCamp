50bd8035a35716579113dea9f2f8ade5945fb167
jakubdaze <jakubdaze@gmail.com>
2014-06-03 13:03:59 +0200

    Day2

--- hello-world/src/main/java/boot/camp/day1/hello/HelloWorld.java
+++ hello-world/src/main/java/boot/camp/day1/hello/HelloWorld.java
@@ -1,5 +1,8 @@
 package boot.camp.day1.hello;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class HelloWorld {
@@ -7,11 +10,16 @@ public class HelloWorld {
     public static void main(String[] args) throws Exception {
         String fp;
     	Scanner filePathReader=new Scanner(System.in);
+    	LinkedList<Person> personalData=new LinkedList<Person>();
     	System.out.print("Podaj sciezke do pliku CVS: ");
     	fp=filePathReader.next();
-    	csvParser parseFile=new csvParser(fp);
-    	
-    	parseFile.preprocessCSVFile();
+    	InputStream fileStream=new FileInputStream(fp);
+    	CSVParser csvFile=new CSVParser();
+    	personalData=csvFile.preprocessCSVFile(fileStream);
+    	for (Person s : personalData) { 
+    		s.printPerson();
+    		System.out.println("");
+    	}
     	filePathReader.close();
     }
 
