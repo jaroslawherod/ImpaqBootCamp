f52339473eabcf879707a985974d485676fbca91
Andriew <andriew1@o2.pl>
2014-06-09 13:19:09 +0200

    servlet - job started

--- maven-writer/src/main/java/com/impaq/app/WriterMain.java
+++ maven-writer/src/main/java/com/impaq/app/WriterMain.java
@@ -9,50 +9,42 @@ import java.io.Reader;
 import java.io.Writer;
 import java.util.List;
 
+public class WriterMain {
+	public WriterMain(  ) throws IOException {
+		String path, patchnewfile;
+		Reader reader = null;
+		Parser person = null;
 
-public class WriterMain 
-{
-    public static void main( String[] args ) throws IOException
-    {
-	String path, patchnewfile;
-	Reader reader = null;
-	Parser person = null;
-	
-	Writer wr = null;
-	SaveCsv writer = null;
-	
-	try {
-	    path = "src//test//resources//sample.csv";
-	    patchnewfile = "src//test//resources//newcsv.csv";
-	    reader = new FileReader( new File(path));
-	    person = new Parser();
-	    List<Container> l = person.parse(reader);
-	    
-	    File check = new File(patchnewfile);
-	    if( check.exists() )
-	    {
-		check.setWritable(true);
-		check.delete();
-	    }
-	    
-	    wr = new FileWriter(patchnewfile);
-	    writer = new SaveCsv();
-	    writer.savetofile(wr, l);
-	    
-	} catch (FormatException exformat) {
-	    System.out.println(exformat.getMessage());
-	} catch (FileNotFoundException fileex) {
-	    System.out.println("Nie znaleziono pliku. "
-	    	+ "Sprawdź czy wprowadzona ścieżka jest poprawna: "
-		    + fileex.getMessage());
-	} catch (Exception e) {
-	    System.out.println(e.getMessage());
-	} finally {
-	    if (reader != null)
-		reader.close();
-	    if(wr != null)
-		wr.close();
+		Writer wr = null;
+		SaveCsv writer = null;
+
+		try {
+			path = "src//test//resources//sample.csv";
+			patchnewfile = "src//test//resources//newcsv.csv";
+			reader = new FileReader(new File(path));
+			person = new Parser();
+			List<Container> l = person.parse(reader);
+
+			File check = new File(patchnewfile);
+
+			wr = new FileWriter(patchnewfile);
+			writer = new SaveCsv();
+			writer.savetofile(wr, l);
+
+		} catch (FormatException exformat) {
+			System.out.println(exformat.getMessage());
+		} catch (FileNotFoundException fileex) {
+			System.out.println("Nie znaleziono pliku. "
+					+ "Sprawdź czy wprowadzona ścieżka jest poprawna: "
+					+ fileex.getMessage());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			if (reader != null)
+				reader.close();
+			if (wr != null)
+				wr.close();
+		}
+
 	}
-	
-    }
 }
