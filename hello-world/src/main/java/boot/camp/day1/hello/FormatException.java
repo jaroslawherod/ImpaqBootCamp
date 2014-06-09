550bbdde5a2f18159a47abb3c890c30a38b9e1c3
Andriew <andriew1@o2.pl>
2014-06-09 10:24:49 +0200

    update

---
+++ hello-world/src/main/java/boot/camp/day1/hello/FormatException.java
@@ -0,0 +1,13 @@
+package boot.camp.day1.hello;
+
+public class FormatException extends Exception
+{
+	public FormatException() {}
+	
+	public FormatException(String message)
+	{
+		super(message);
+	}
+	
+	
+}
