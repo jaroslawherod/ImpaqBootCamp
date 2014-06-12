b8ca391f91d413536eb728a1dbb2e7444c944256
jakubdaze <jakubdaze@gmail.com>
2014-06-12 14:20:07 +0200

    MVC

--- CSVWeb/src/main/java/com/web/json/Controller.java
+++ CSVWeb/src/main/java/com/web/json/Controller.java
@@ -23,43 +23,17 @@ public class Controller extends HttpServlet{
 	
 	private static final long serialVersionUID = 1L;
     
-	public Person validJson(BufferedReader br) throws IOException{
-		Gson gson=new Gson();
-		try{
-			return gson.fromJson(br, Person.class);
-		}
-		catch(com.google.gson.JsonSyntaxException e){
-			throw new JsonException("Nieprawidlowy format danych",e);
-		}
-	}
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Model model=new Model();
 		response.setCharacterEncoding("UTF-8");
-		String path = System.getProperty("user.home");
-		String fullPath=path+"/inputdata.csv";
-		try{
-		InputStream file=new FileInputStream(fullPath);
 		PrintWriter out=response.getWriter();
-		Gson gson=new Gson();
-		CSVParser csvParser=new CSVParser();
-		List <Person> personsList=csvParser.preprocessCSVFile(file);
-		out.write(gson.toJson(personsList));
-		out.flush();
-		out.close();
-		}
-		catch(FileNotFoundException e){
-			throw new CSVException("Brak pliku inputdata.csv w katalogu uzytkownika", e);
-		}
+		model.printJson(out);
     }	
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		BufferedReader readStream=request.getReader();
-		CSVWriter csvWriteFile=new CSVWriter();
-		String path = System.getProperty("user.home");
-		String fullPath=path+"/inputdata.csv";
-		List<Person> personsList=new LinkedList<Person>();
-		OutputStream fileWriter=new FileOutputStream(fullPath,true);
-		personsList.add(validJson(readStream));
-		csvWriteFile.writePersonsToStream(fileWriter, personsList);
+		Model model=new Model();
+		model.addPerson(readStream);
 	}	
 }
